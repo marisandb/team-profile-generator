9feec1f83592d29a48cf4da6485cec7cb2f7a7a7
maris <marisandb@yahoo.com>
2021-08-05 23:09:41 -0500

    added more Employee tests and ensured they all pass

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee')
 
 test('creats employee object', () => {
@@ -7,3 +8,15 @@ test('creats employee object', () => {
     expect(employee.id).toEqual(expect.any(Number))
     expect(employee.email).toEqual(expect.any(String))
 });
+
+test('returns employee name', () => {
+    const employee = new Employee('Bobbert', 35, 'bobbert@bob.com');
+
+    expect(employee.getName()).toEqual(expect.any(String))
+})
+
+test('returns employee ID', () => {
+    const employee = new Employee('Bobbert', 35, 'bobbert@bob.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number))
+})
